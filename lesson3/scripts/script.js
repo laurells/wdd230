9da5cd1ec1ba60f24644d26346a5bf8d29c9a67c
Laurels Echichinwo <laurels.echichinwo@stu.cu.edu.ng>
2022-10-02 12:26:53 +0100

    Web planner

--- lesson3/scripts/script.js
+++ lesson3/scripts/script.js
@@ -1,6 +1,6 @@
 let current_date = new Date();
 let current_year = current_date.getFullYear();
 
-document.querySelector("span").textContent = current_year;
+document.querySelector("year").innerHTML = current_year;
 
-document.getElementById("updated").textContent = "Last Updated: " + document.lastModified;
+document.getElementById("updated").innerHTML = "Last Updated: " + document.lastModified;

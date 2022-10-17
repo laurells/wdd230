fe251ac91cc162e28144dd9f4ed3487b7b78fd52
Laurels <laurels.echichinwo@stu.cu.edu.ng>
2022-10-17 21:01:26 +0100

    New changes made on website layout

--- chamber/scripts/main.js
+++ chamber/scripts/main.js
@@ -6,26 +6,15 @@ const currentDate = new Intl.DateTimeFormat("en-UK", {
   document.querySelector("#currentDate").innerHTML = currentDate;
 
 
-/* Hamburger Button */
-/*unction toggleMenu () {
+ /*Hamburger Button */
+function toggleMenu () {
     document.getElementById("primary-nav").classList.toggle("open");
-    document.getElementById("hamburger-btn").classList.toggle("open");
+    document.getElementById("hamburgerBtn").classList.toggle("open");
 }
 
-const x = document.getElementById("hamburger-btn");
+const x = document.getElementById("hamburgerBtn");
 x.onclick = toggleMenu;
 
 const date = new Date(document.lastModified).toLocaleString();
 
-document.querySelector("#last-updated").innerHTML = date;*/
-
-
-/* Monday and Tuesday Message */
-const dayNumber = now.getDay();
-const message = document.getElementById("message");
-
-if ((dayNumber == 1) || (dayNumber == 2)) {
-    message.classList.add("showme");
-} else {
-    message.classList.add("hideme");
-};
+document.querySelector("#last-updated").innerHTML = date;

24c6419950b92decf42d6dbc1f3e14678328cb77
Laurels <laurels.echichinwo@stu.cu.edu.ng>
2022-11-10 17:02:19 +0000

    join.html file added

--- chamber/scripts/main.js
+++ chamber/scripts/main.js
@@ -1,15 +1,15 @@
 /* Formatted Date Header */
 const currentDate = new Intl.DateTimeFormat("en-UK", {
-    dateStyle: "full",
-  }).format(new Date());
-  
-  document.querySelector("#currentDate").innerHTML = currentDate;
+  dateStyle: "full",
+}).format(new Date());
+
+document.querySelector("#currentDate").innerHTML = currentDate;
 
 
- /*Hamburger Button */
-function toggleMenu () {
-    document.getElementById("primary-nav").classList.toggle("open");
-    document.getElementById("hamburgerBtn").classList.toggle("open");
+/*Hamburger Button */
+function toggleMenu() {
+  document.getElementById("primary-nav").classList.toggle("open");
+  document.getElementById("hamburgerBtn").classList.toggle("open");
 }
 
 const x = document.getElementById("hamburgerBtn");
@@ -20,8 +20,15 @@ const date = new Date(document.lastModified).toLocaleString();
 document.querySelector("#last-updated").innerHTML = date;
 
 const hero = new Date();
-const  day=hero.getDay();
+const day = hero.getDay();
 if (day < 1 || day > 2) {
-    const x = document.querySelector('.topbar');
-    x.remove();
-  }
+  const x = document.querySelector('.topbar');
+  x.remove();
+}
+
+const joinElement = document.getElementById('join');
+if (joinElement) {
+  joinElement.onclick = function () {
+    location.href = 'https://laurells.github.io/wdd230/chamber/join.html';
+  };
+}
